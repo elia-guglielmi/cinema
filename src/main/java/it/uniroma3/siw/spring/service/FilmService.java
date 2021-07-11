d51b723385955216870768a012986a19695e01ff
elia-guglielmi <72272747+elia-guglielmi@users.noreply.github.com>
2021-07-11 16:11:22 +0200

    .

--- src/main/java/it/uniroma3/siw/spring/service/FilmService.java
+++ src/main/java/it/uniroma3/siw/spring/service/FilmService.java
@@ -1,10 +1,13 @@
 package it.uniroma3.siw.spring.service;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 
+import org.hibernate.criterion.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +23,21 @@ public class FilmService {
 	public List<Film> tutti(){
 		return (List<Film>)filmRepository.findAll();
 	}
+	
+	@Transactional
+	public List<Film> tuttiOrdinati(){
+		 List<Film> ordinati=(List<Film>)filmRepository.findAll();
+		 Collections.sort(ordinati,new Comparator<Film>() {
+
+			@Override
+			public int compare(Film o1, Film o2) {
+				int cmp=o1.getTitolo().compareTo(o2.getTitolo());
+				return cmp;
+			}
+		});
+		 return ordinati;
+	}
+	
 	
 	@Transactional
 	public List<Film> fillmPerTitolo(String titolo){
